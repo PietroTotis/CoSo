78196dae038645d74e1df321130e06705b53a2c9
PietroTotis <pietro.totis@student.kuleuven.be>
2022-03-23 16:34:06 +0100

    fixed paths in run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,24 +3,25 @@
 
 py="pyenv/bin/python3.8"
 source pyenv/bin/activate
+path=realpath .
 
 # asp
-$py src/tester.py --asp --test-folder tests/benhmarks/sequence > tests/results/sequences_asp.txt
-$py src/tester.py --asp --test-folder tests/benhmarks/permutation > tests/results/permutations_asp.txt
-$py src/tester.py --asp --test-folder tests/benhmarks/subset > tests/results/subsets_asp.txt
-$py src/tester.py --asp --test-folder tests/benhmarks/multisubset > tests/results/multisubsets_asp.txt
-$py src/tester.py --asp --test-folder tests/benhmarks/composition > tests/results/compositions_asp.txt
+$py src/tester.py --asp --test-folder tests/benhmarks/sequence > "$path/tests/results/sequences_asp.txt"
+$py src/tester.py --asp --test-folder tests/benhmarks/permutation > "$path/tests/results/permutations_asp.txt"
+$py src/tester.py --asp --test-folder tests/benhmarks/subset > "$path/tests/results/subsets_asp.txt"
+$py src/tester.py --asp --test-folder tests/benhmarks/multisubset > "$path/tests/results/multisubsets_asp.txt"
+$py src/tester.py --asp --test-folder tests/benhmarks/composition > "$path/tests/results/compositions_asp.txt"
 
 # #sat
-$py src/tester.py --sat --test-folder tests/benhmarks/sequence > tests/results/sequences_sat.txt
-$py src/tester.py --sat --test-folder tests/benhmarks/permutation > tests/results/permutations_sat.txt
-$py src/tester.py --sat --test-folder tests/benhmarks/subset > tests/results/subsets_sat.txt
-$py src/tester.py --sat --test-folder tests/benhmarks/multisubset > tests/results/multisubsets_sat.txt
-$py src/tester.py --sat --test-folder tests/benhmarks/composition > tests/results/compositions_sat.txt
+$py src/tester.py --sat --test-folder tests/benhmarks/sequence > "$path/tests/results/sequences_sat.txt"
+$py src/tester.py --sat --test-folder tests/benhmarks/permutation > "$path/tests/results/permutations_sat.txt"
+$py src/tester.py --sat --test-folder tests/benhmarks/subset > "$path/tests/results/subsets_sat.txt"
+$py src/tester.py --sat --test-folder tests/benhmarks/multisubset > "$path/tests/results/multisubsets_sat.txt"
+$py src/tester.py --sat --test-folder tests/benhmarks/composition > "$path/tests/results/compositions_sat.txt"
 
 # essence
-$py src/tester.py --essence --test-folder tests/benhmarks/sequence > tests/results/sequences_essence.txt
-$py src/tester.py --essence --test-folder tests/benhmarks/permutation > tests/results/permutations_essence.txt
-$py src/tester.py --essence --test-folder tests/benhmarks/subset > tests/results/subsets_essence.txt
-$py src/tester.py --essence --test-folder tests/benhmarks/multisubset > tests/results/multisubsets_essence.txt
-$py src/tester.py --essence --test-folder tests/benhmarks/composition > tests/results/compositions_essence.txt
+$py src/tester.py --essence --test-folder tests/benhmarks/sequence > "$path/tests/results/sequences_essence.txt"
+$py src/tester.py --essence --test-folder tests/benhmarks/permutation > "$path/tests/results/permutations_essence.txt"
+$py src/tester.py --essence --test-folder tests/benhmarks/subset > "$path/tests/results/subsets_essence.txt"
+$py src/tester.py --essence --test-folder tests/benhmarks/multisubset > "$path/tests/results/multisubsets_essence.txt"
+$py src/tester.py --essence --test-folder tests/benhmarks/composition > "$path/tests/results/compositions_essence.txt"
